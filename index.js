739511c29c6ae28815b3c10df55d919ed04eca38
Alen <alensbastias@gmail.com>
2023-03-18 00:21:59 -0300

    se borra el pgport de index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const app = require('./src/app');
-const port = process.env.PGPORT || 3000;
+const port = 3000;
 app.listen(port, () => console.log(`La aplicacion fue exitosamente desplegada en el puerto:${port}`))
 
