074d957da07b6780d5062599293ee6bba98cf068
Juan <juan.lozada1905@gmail.com>
2023-03-17 22:33:24 -0300

    pull desde el main 2

--- src/function/functions.js
+++ src/function/functions.js
@@ -2,11 +2,18 @@ const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const { Pool } = require("pg");
 
+const host = process.env.PGHOST || process.env.DB_HOST
+const user = process.env.PGUSER || process.env.DB_USER
+const database = process.env.PGDATABASE || process.env.DB_NAME
+const password = process.env.PGPASSWORD || process.env.DB_PASSWORD
+const name = process.env.DB_NAME
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: host,
+  user: user,
+  name: name,
+  password: password,
+  database: database,
   allowExitOnIdle: true,
 });
 
